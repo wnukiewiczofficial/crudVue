7da0ac3b2a49a98eea9c02ea26361486a2e97001
wnukiewiczofficial <wnukiewiczofficial@yahoo.com>
2024-04-12 00:10:38 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <h1>Post CRUD Application</h1>
+  </header>
+  <main class="d-flex flex-column pa-6 ga-3">
+    <div :key="post.id" v-for="post in posts">
+      <PostCard :title="post.title" :content="post.content" />
+    </div>
+  </main>
+</template>
+
+<script>
+import PostCard from "./components/PostCard.vue";
+export default {
+  name: "App",
+  components: { PostCard },
+  data() {
+    return {
+      posts: [{ id: 0, title: "Title", content: "Content text" }],
+    };
+  },
+  methods: {
+    addPost(title, content) {
+      this.posts.push({ id: this.posts.length, title, content });
+    },
+    deletePost() {
+      // Here deleting
+    },
+  },
+  mounted() {
+    this.addPost("Siema", "Pedaly");
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PostCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card
+    class="text-left"
+    color="indigo"
+    variant="elevated"
+    :title="title"
+    :text="content"
+  >
+    <v-card-actions>
+      <v-spacer />
+      <v-btn> Delete </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "PostCard",
+  props: {
+    title: String,
+    content: String,
+  },
+};
+</script>
